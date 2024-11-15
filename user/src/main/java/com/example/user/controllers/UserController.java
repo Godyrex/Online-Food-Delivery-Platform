92ef4aeb012d3579a4b43069f92b206d799fce8e
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2024-11-15 14:37:09 +0100

    fixed few issues

--- user/src/main/java/com/example/user/controllers/UserController.java
+++ user/src/main/java/com/example/user/controllers/UserController.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Enumeration;
-import java.util.List;
+
 import java.util.Map;
 
 @RestController
